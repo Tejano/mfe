74d2a315ea932cead5b7bc0f3599f012065a3c74
Tejano <hescobedo@hotmail.com>
2021-12-21 12:45:04 -0600

    Added Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 //import mount from 'marketing/MarketingApp';
 //console.log(mount);
 //test
 export default () => {
   return (
-    <div>
-      <h1>Hi There!!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
